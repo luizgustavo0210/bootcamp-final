c887e86c12ea9594109dbc25524e25e52108dde5
luizgustavo0210 <eletrica2019anderson@gmail.com>
2024-12-02 14:35:19 -0300

    teste

---
+++ projeto/script.js
@@ -0,0 +1,23 @@
+document.getElementById('imcForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const peso = parseFloat(document.getElementById('peso').value);
+    const altura = parseFloat(document.getElementById('altura').value);
+    const sexo = document.getElementById('sexo').value;
+    const idade = parseInt(document.getElementById('idade').value);
+
+    const imc = peso / (altura * altura);
+    let classificacao = '';
+
+    if (imc < 18.5) {
+        classificacao = 'Abaixo do peso';
+    } else if (imc >= 18.5 && imc < 24.9) {
+        classificacao = 'Peso normal';
+    } else if (imc >= 25 && imc < 29.9) {
+        classificacao = 'Sobrepeso';
+    } else {
+        classificacao = 'Obesidade';
+    }
+
+    document.getElementById('resultado').innerHTML = `Seu IMC é ${imc.toFixed(2)}. Classificação: ${classificacao}`;
+});
